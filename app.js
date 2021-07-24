8dd10a4215eb932843314c0760a8d1320a5fcb74
Kseniya <kseniyamudrakovadev@gmail.com>
2021-07-24 23:40:49 +0200

    ajout mouvement aliens

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const grid = document.querySelector(".grid");
 let currentShooterIndex = 200;
 let width = 25;
+let direction = 1;
+let invaderId;
 
 let aliens = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -24,6 +26,12 @@ function draw() {
 
 draw();
 
+function remove() {
+  for (let i = 0; i < aliens.length; i++) {
+    square[aliens[i]].classList.remove("invader");
+  }
+}
+
 square[currentShooterIndex].classList.add("shooter");
 
 function moveShooter(e) {
@@ -40,3 +48,17 @@ function moveShooter(e) {
 }
 
 document.addEventListener("keydown", moveShooter);
+
+function moveAlien() {
+  const leftEdge = aliens[0] % width === 0;
+  const rightEdge =
+  aliens[aliens.length - 1] % width === width - 1;
+  remove();
+
+  for (let i = 0; i < aliens.length; i++) {
+    aliens[i] += direction;
+  }
+  draw()
+}
+
+invaderId = setInterval(moveAlien, 500)
